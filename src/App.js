93cf436a4f1806138b439e82f1b1e488e5728a4d
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 11:27:14 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon"
+    
+  ];
+  const [filter, setFilter] = useState(list)
+
+  const search = (event) => {
+    
+    if (event.target.value === "") {
+      setFilter(list)
+      return
+    }
+    const filterSearch = list.filter((item) => 
+      item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    )
+    setFilter(filterSearch)
+  }
+
+  return <>
+  <div>
+      Search <input name="query" type="text" onChange={search} />
+  </div>
+  <div>
+    {filter.map((fruit, key) => ( 
+      <div key={key}>{fruit}</div>
+    ))}
+  </div>
+  </>
 }
 
 export default App;
