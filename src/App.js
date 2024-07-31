704aa6511272cb1f368dcafecde49f41294938fe
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 16:37:19 +0300

    Task 2 update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-
-function App() {
-  let input, number, str;
-  str = "number 1:";
-  while (true) {
-      input = prompt(str);
-      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
-          alert("введено не число");
-      else break;
+function TextClass() {
+    const str1 = "Java";
+    const str2 = "Java";
+    const str3 = "C++";
+   
+   
+    if (str1.equals(str2)) {
+      alert("Statement is true");
+    } else {
+      alert("Statement is false");
+    }
+   
+    if(str1.equals(str3)) {
+      alert("Statement is true");
+    } else {
+      alert("Statement is false");
+    }
   }
-  let num1 = number;
-
-  str = "number 2:";
-  while (true) {
-      input = prompt(str);
-      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
-          alert("введено не число");
-      else break;
-  }
-  let num2 = number;
-
-  if (num1 > num2) {
-      alert("Первое число больше второго");
-  }
-  else if (num1 < num2) {
-      alert("Второе число больше первого");
-  }
-  else {
-      alert("числа равны");
-  }
-}
-
-export default App;
+  export default TextClass
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import TestClass from './TextCheck'
+import TextClass from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
-    <TestClass/>
+    <TextClass/>
   </React.StrictMode>
 );
 
