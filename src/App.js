a1f92e29f3012bba50bd84df383ac439f9820bca
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 17:00:02 +0300

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import {useState} from 'react'
 
 function App() {
-    const [array, setArray] = useState([1,2,3,4,5,6,7,8,9,10]);
-    const newArray = () => setArray(array => array.filter(x => x % 2))
-    return<div>
-        {
-            array.map(item => <p key={item}>{item}</p>)
-        }
-        <button onClick={newArray}>Отфлитровать</button>
-    </div>
-
-const str1 = "Java";
-const str2 = "Java";
-const str3 = "C++";
-
-
-if (str1.equals(str2)) {
-  alert("Statement is true");
-} else {
-  alert("Statement is false");
-}
-
-if(str1.equals(str3)) {
-  alert("Statement is true");
-} else {
-  alert("Statement is false");
-}
-
-
-let input, number, str, text;
+let input, number, str;
   str = "number 1:";
   while (true) {
       input = prompt(str);
