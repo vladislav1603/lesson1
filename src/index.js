573921caabed513378e848cdf2a42f4da575c3b5
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 16:37:13 +0300

    Task 1 update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TestClass from './TextCheck'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-    <TestClass/>
   </React.StrictMode>
 );
 
