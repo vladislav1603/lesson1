f8b5620d8d900d4a6b3ba4f558eebc2f6dfbce13
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 16:38:30 +0300

    Tusk 3 update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
+import {useState} from 'react'
 
 function App() {
-  let input, number, str;
-  str = "number 1:";
-  while (true) {
-      input = prompt(str);
-      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
-          alert("введено не число");
-      else break;
-  }
-  let num1 = number;
-
-  str = "number 2:";
-  while (true) {
-      input = prompt(str);
-      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
-          alert("введено не число");
-      else break;
-  }
-  let num2 = number;
-
-  if (num1 > num2) {
-      alert("Первое число больше второго");
-  }
-  else if (num1 < num2) {
-      alert("Второе число больше первого");
-  }
-  else {
-      alert("числа равны");
-  }
+    const [array, setArray] = useState([1,2,3,4,5,6,7,8,9,10]);
+    const newArray = () => setArray(array => array.filter(x => x % 2))
+    return<div>
+        {
+            array.map(item => <p key={item}>{item}</p>)
+        }
+        <button onClick={newArray}>Отфлитровать</button>
+    </div>
 }
+
 
 export default App;
