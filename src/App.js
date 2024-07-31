b9cbe44fc0fc628e278c6831058d971744f8d7d8
vladislav1603 <institute.programirovanie@gmail.com>
2024-07-31 16:54:04 +0300

    234

--- src/App.js
+++ src/App.js
@@ -9,6 +9,53 @@ function App() {
         }
         <button onClick={newArray}>Отфлитровать</button>
     </div>
+
+const str1 = "Java";
+const str2 = "Java";
+const str3 = "C++";
+
+
+if (str1.equals(str2)) {
+  alert("Statement is true");
+} else {
+  alert("Statement is false");
+}
+
+if(str1.equals(str3)) {
+  alert("Statement is true");
+} else {
+  alert("Statement is false");
+}
+
+
+let input, number, str, text;
+  str = "number 1:";
+  while (true) {
+      input = prompt(str);
+      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
+          alert("введено не число");
+      else break;
+  }
+  let num1 = number;
+
+  str = "number 2:";
+  while (true) {
+      input = prompt(str);
+      if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
+          alert("введено не число");
+      else break;
+  }
+  let num2 = number;
+
+  if (num1 > num2) {
+      alert("Первое число больше второго");
+  }
+  else if (num1 < num2) {
+      alert("Второе число больше первого");
+  }
+  else {
+      alert("числа равны");
+  }
 }
 
 export default App;
